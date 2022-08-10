6dd6ab733f6cc78958537901ef3d9e05fba6e69a
ASR <pekoron@gmail.com>
2022-08-10 17:08:02 +0300

    +

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -44,17 +44,17 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
 fun daysInMonth(month: Int, year: Int): Int {
-    if (month == 2)  // високосный год
-        if (year % 400 == 0 || year % 4 == 0)
-            return 29
-        else return 28
-
+    if (month == 2 && year % 4 == 0) { // високосный год
+        if (year % 400 != 0)
+            return 28
+        else return 29
+    }
 
     if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
         return 31
-    if (month == 4 || month == 6 || month == 9 || month == 11)
-        return 30
-    else return 28
+    // if (month == 4 || month == 6 || month == 9 || month == 11)
+    //     return 30
+    else return 30
 }
 
 /**
